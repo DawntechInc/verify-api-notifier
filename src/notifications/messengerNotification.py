60d0e508f4264c347dd6beb75c003a8cec64cbf3
Levindo Neto <levindogtn@gmail.com>
2019-01-18 14:07:59 -0200

    Modify recipient id within the notifications

--- src/notifications/messengerNotification.py
+++ src/notifications/messengerNotification.py
@@ -11,13 +11,14 @@ def notifyUser():
   }
   data = {
     "recipient": {
-      "id": <PAGE-SCOPED-ID-USER-HERE>
+      "id": 123456789
     },
     "message": {
       "text": "API has stopped working :("
     },
     "messaging_type":"RESPONSE"
   }
+  # 123456789: <PAGE-SCOPED-ID-USER-HERE>
 
   requests.post(
       url="https://graph.facebook.com/v2.6/me/messages?access_token=<PAGE_ACCESS_TOKEN_HERE>",
